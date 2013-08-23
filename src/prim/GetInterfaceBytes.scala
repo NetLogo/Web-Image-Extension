f95083ac5e375616aac423e701d57f6ebce71a9f
TheBizzle <jason.bertsche@gmail.com>
2013-08-22 22:02:05 -0500

    Using a less-dependent way of getting ahold of the 'Applet' component

--- src/prim/GetInterfaceBytes.scala
+++ src/prim/GetInterfaceBytes.scala
@@ -1,17 +1,21 @@
 package org.nlogo.extensions.webimage.prim
 
 import
-  org.nlogo.{ api, app, awt },
+  java.awt.Component
+
+import
+  org.nlogo.{ api, awt, nvm, window },
     api.{ Argument, Context, DefaultReporter, Syntax },
       Syntax.WildcardType,
-    app.App,
-    awt.Images
+    awt.Images,
+    nvm.ExtensionContext,
+    window.GUIWorkspace
 
 object GetInterfaceBytes extends DefaultReporter {
   override def getSyntax = Syntax.reporterSyntax(WildcardType)
   override def report(args: Array[Argument], context: Context): AnyRef = {
-    // See the Web extension's `ExportInterface` code for justification for this grossness --JAB (8/21/13)
-    val component = App.app.tabs.interfaceTab.getInterfacePanel
+    // See the Web extension's `ExportInterface` code for justification for this (somewhat different) grossness --JAB (8/21/13)
+    val component = context.asInstanceOf[ExtensionContext].workspace.asInstanceOf[GUIWorkspace].getWidgetContainer.asInstanceOf[Component]
     Images.paintToImage(component).toByteArray
   }
 }
