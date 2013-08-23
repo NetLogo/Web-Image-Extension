c7cab64b535472380b1fc644d4f8155a72da05cc
TheBizzle <jason.bertsche@gmail.com>
2013-08-22 22:01:12 -0500

    Adding optional 'override' specifier

--- src/WebImageExtension.scala
+++ src/WebImageExtension.scala
@@ -7,7 +7,7 @@ import
   prim._
 
 class WebImageExtension extends DefaultClassManager {
-  def load(primitiveManager: PrimitiveManager) {
+  override def load(primitiveManager: PrimitiveManager) {
     primitiveManager.addPrimitive("get-view-bytes",      GetViewBytes)
     primitiveManager.addPrimitive("get-interface-bytes", GetInterfaceBytes)
     primitiveManager.addPrimitive("base64-encode",       Base64Encode)
